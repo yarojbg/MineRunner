51b9f737046dcab42e2e92cc4dbfb7658a81c436
yarojbg <yarojbg@gmail.com>
2023-11-15 18:36:49 +0200

    fixed ui ws left enabled after button increase

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
