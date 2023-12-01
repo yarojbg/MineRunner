b1ff77c23ee604faf7b89a2b23efcace5b3eda9a
yarojbg <yarojbg@gmail.com>
2023-12-01 21:50:52 +0200

    replaced .time with .fixedtime

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -9,19 +9,19 @@ public class Explosion : MonoBehaviour
 
 	void Start()
 	{
-		_startTime = Time.time;
+		_startTime = Time.fixedTime;
 	}
 
 	void Update()
 	{
-		transform.localScale = (Time.time - _startTime) * explosionSpeed * Vector3.one;
+		transform.localScale = (Time.fixedTime - _startTime) * explosionSpeed * Vector3.one;
 	}
 
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.gameObject.layer == Layers.Player)
 			return;
-		float forceMultiplier = 1 / ((Time.time - _startTime) * (Time.time - _startTime));
+		float forceMultiplier = 1 / ((Time.fixedTime - _startTime) * (Time.fixedTime - _startTime));
 		if (other.gameObject.TryGetComponent(out Rigidbody rb))
 			rb.velocity = (other.transform.position - transform.position) * forceMultiplier;
 	}
